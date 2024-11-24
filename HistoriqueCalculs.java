c87fa13d5befee4c2cd0990d2f61993c45af62e3
babyfacedassassinat <mamadouissadiedhiou@gmaile.com>
2024-11-24 23:40:14 +0000

    Ajout de nouveaux fichiers ou modifications

--- HistoriqueCalculs.java
+++ HistoriqueCalculs.java
@@ -13,17 +13,15 @@ public class HistoriqueCalculs {
 
     public void ajouterCalcul(AdresseIP adresse) {
         historique.add(adresse);
-        sauvegarderCalculDansFichier("output/historique.txt", adresse);  
+        sauvegarderNouvelleAdresseDansFichier("output/historique.txt", adresse);  
     }
 
-    public List<AdresseIP> getHistorique() {
-        return historique;
-    }
-
-    private void sauvegarderCalculDansFichier(String nomFichier, AdresseIP adresse) {
+    private void sauvegarderNouvelleAdresseDansFichier(String nomFichier, AdresseIP adresse) {
         try (FileWriter writer = new FileWriter(nomFichier, true)) { 
-            writer.write(adresse.toString() + "\n");
-            System.out.println("Adresse ajoutée à l'historique : " + adresse.toString());
+            writer.write("Adresse IP : " + adresse.toString() + "\n");
+            writer.write("Adresse réseau : " + adresse.getAdresseReseau() + "\n");
+            writer.write("Plage d'adresses : " + String.join(" - ", adresse.getPlageAdresses()) + "\n");
+            writer.write("======================================\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,10 +30,10 @@ public class HistoriqueCalculs {
     public void sauvegarderHistorique(String nomFichier) {
         System.out.println("Enregistrement de l'historique complet dans : " + nomFichier);
         File file = new File(nomFichier);
-        
+
         if (!file.exists()) {
             try {
-                file.createNewFile(); // Crée un nouveau fichier
+                file.createNewFile();
                 System.out.println("Fichier créé avec succès : " + nomFichier);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -44,11 +42,18 @@ public class HistoriqueCalculs {
     
         try (FileWriter writer = new FileWriter(file)) {
             for (AdresseIP adresse : historique) {
-                writer.write(adresse.toString() + "\n");
+                writer.write("Adresse IP : " + adresse.toString() + "\n");
+                writer.write("Adresse réseau : " + adresse.getAdresseReseau() + "\n");
+                writer.write("Plage d'adresses : " + String.join(" - ", adresse.getPlageAdresses()) + "\n");
+                writer.write("======================================\n");
             }
             System.out.println("Historique complet sauvegardé.");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<AdresseIP> getHistorique() {
+        return historique;
     }
 }
